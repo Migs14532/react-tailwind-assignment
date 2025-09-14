e1909d5866ebf5a0bc494343d416892ada2d1c04
Migs14532 <miguelpaolobarafon.202301454@gmail.com>
2025-09-14 23:01:04 +0800

    Update Footer.js

--- profile-assignment/src/components/Footer.js
+++ profile-assignment/src/components/Footer.js
@@ -3,9 +3,10 @@ function Footer() {
     <footer className="bg-gray-800 text-white p-4 text-center">
       <p>© 2025 My Website.</p>
       <div className="flex justify-center text-lg">
-        <a href="https://www.facebook.com/miguel.barafon" className="hover:text-red-500">Facebook</a>
+        <a href="https://www.facebook.com/miguel.barafon" className="hover:text-red-500 cursor-pointer">Facebook</a>
       </div>
     </footer>
   );
 }
 export default Footer;
+
